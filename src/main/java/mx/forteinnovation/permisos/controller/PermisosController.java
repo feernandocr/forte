f1ff86e83093babe97817c07560a9b7ee3b5929c
Mario Fernando Cortez De La Rosa <feernando.cortees@gmail.com>
2022-03-25 14:12:12 -0600

    Fix nombres id

--- src/main/java/mx/forteinnovation/permisos/controller/PermisosController.java
+++ src/main/java/mx/forteinnovation/permisos/controller/PermisosController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/permisos")
 public class PermisosController {
 
